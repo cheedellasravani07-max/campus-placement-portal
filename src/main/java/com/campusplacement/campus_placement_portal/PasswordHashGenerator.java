057dccd6edc1696fbac1d5c7032a4d8ba18b30ae
cheedellasravani07-max <cheedellasravani07@gmail.com>
2026-09-20 11:58:31 +0530

    Prepare project for deployment

---
+++ src/main/java/com/campusplacement/campus_placement_portal/PasswordHashGenerator.java
@@ -0,0 +1,19 @@
+package com.campusplacement.campus_placement_portal;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordHashGenerator {
+
+    public static void main(String[] args) {
+
+        BCryptPasswordEncoder encoder =
+                new BCryptPasswordEncoder();
+
+        String password = "Student@123";
+
+        String hash = encoder.encode(password);
+
+        System.out.println("Password: " + password);
+        System.out.println("BCrypt Hash: " + hash);
+    }
+}
